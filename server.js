7da7fb7082d6f29d06bba82e731c3fc5a242e4cf
Northerchild <osarus-13@hotmail.com>
2018-11-06 16:17:56 -0500

    Se crean los hbs del proyecto y listo para subir a heroku n_n

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const app = express()
 const hbs = require('hbs');
 require('./hbs/helpers');
 
-const port = process.env.port 3000;
+const port = 3000;
 
 app.use(express.static(__dirname + '/public'))
 //Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales')
 app.set('view engine', 'hbs');
 
+app.get('/',(req, res)=> {
+  //res.send('HolaData')
+	res.render('home',{
+	});
+});
 
 if (!port) {
 	console.error("No se definio bien el puerto")
 }else{
 	console.log(`Escuchando desde el puerto ${port}`)	
 }
+
+app.listen(3000,()=>{
+
+});
