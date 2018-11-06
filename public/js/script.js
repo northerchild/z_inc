bb5e66212fe91b3373dde177a467daf4a131b289
Northerchild <osarus-13@hotmail.com>
2018-11-05 19:40:43 -0500

    Se crea la funcion toggle del menu hamburguesa

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-/*Preloader*/
-$(window).on('load',()=>{
-	$('#status').fadeOut();
-	$('#preloader').delay(2000).fadeOut('slow');
-})
+let mobile_nav_in = document.getElementById("mobile-nav-open-btn").addEventListener("click", 
+	()=>{let click = document.getElementById("mobile-nav").style.height = '100%' });
+
+let mobile_nav_out = document.getElementById("mobile-nav").addEventListener("click", 
+	()=>{let click = document.getElementById("mobile-nav").style.height = '0%' });
